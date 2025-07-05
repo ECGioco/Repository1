fe898d3b9bfbb9aa2096339d6382be93a9a73a9b
Everett <ecg605@gmail.com>
2025-07-05 14:49:58 -0700

    Add median function

--- Hello_World2.py
+++ Hello_World2.py
@@ -1,1 +1,9 @@
 print("Hello from Both")
+def median(numbers):
+    numbers = sorted(numbers)
+    n = len(numbers)
+    mid = n // 2
+    if n % 2 == 0:
+        return (numbers[mid - 1] + numbers[mid]) / 2
+    else:
+        return numbers[mid]
